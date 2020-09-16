33f6101caecabe849f28d6a5dd3d3d2a95136d4f
evgeny <evgeny.podjachev@gmail.com>
2020-09-17 00:59:54 +0300

    Added tone.

--- app/src/main/java/android/example/toys/Sounder2.kt
+++ app/src/main/java/android/example/toys/Sounder2.kt
@@ -6,7 +6,8 @@ import android.media.AudioFormat
 import android.media.AudioTrack
 import android.os.Build
 import androidx.annotation.RequiresApi
-import kotlin.experimental.and
+import kotlin.math.exp
+import kotlin.math.sin
 
 @RequiresApi(Build.VERSION_CODES.M)
 class Sounder2 internal constructor(activity: Activity) : ISounder {
@@ -18,7 +19,6 @@ class Sounder2 internal constructor(activity: Activity) : ISounder {
 
 	private val freqOfTone = 440.0 // hz
 	private val sample = DoubleArray(numSamples)
-	private val generatedSnd = ByteArray(2 * numSamples)
 
 	override fun play(clickType: ClickType) {
 		when (clickType) {
@@ -51,41 +51,18 @@ class Sounder2 internal constructor(activity: Activity) : ISounder {
 			.setBufferSizeInBytes(minSize)
 			.build()
 
-		val samples = DoubleArray(numSamples)
-		var t = 0.0;
-		for (i in 0 until numSamples) {
-			//samples[i] = Math.sin(2 * Math.PI * i / (sampleRate / 5)) // Sine wave
-			samples[i] = Math.sin(t) // Sine wave
-			t += 0.5;
-
-			buffer[i] =(samples[i] * Short.MAX_VALUE).toShort() // Higher amplitude increases volume
-			if(i < 241000){
-				//buffer[i] = 30000
-			}
-			//buffer[i] = (samples[241000] * Short.MAX_VALUE).toShort()
-		}
 		genTone()
 		audioTrack.play()
-//		audioTrack.write(buffer, 0, buffer.size, AudioTrack.WRITE_BLOCKING);
-		audioTrack.write(generatedSnd, 0, generatedSnd.size);
+		audioTrack.write(buffer, 0, buffer.size);
 	}
 
 	fun genTone() {
-		// fill out the array
 		for (i in 0 until numSamples) {
-			sample[i] = Math.sin(2 * Math.PI * i / (sampleRate / freqOfTone))
+			sample[i] = sin((2 * Math.PI - .001) * i / (sampleRate / freqOfTone)) * (1 / exp(i.toDouble() / 10000))
 		}
 
-		// convert to 16 bit pcm sound array
-		// assumes the sample buffer is normalised.
-		var idx = 0
-		for (dVal in sample) {
-			// scale to maximum amplitude
-			val v = (dVal * 32767).toInt().toShort()
-			//val v = (0.8 * 32767).toInt().toShort()
-			// in 16 bit wav PCM, first byte is the low order byte
-			generatedSnd[idx++] = (v and 0x00ff).toByte()
-			generatedSnd[idx++] = ((v and 0xff00.toShort()).toInt() shr 8).toByte()
+		for (i in 0 until numSamples) {
+			buffer[i] = (sample[i] * Short.MAX_VALUE).toInt().toShort()
 		}
 	}
 }
